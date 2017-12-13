9e5f21ec48ed8b2739e067465e231d1750bc02e3
Alex Dinu <adix64@gmail.com>
2017-12-13 22:40:46 +0200

    fist commit

---
+++ Assets/RopeBitScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class RopeBitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HAI IN BASCULANTA!");
+
+        ThirdPersonCharacter hull = other.gameObject.GetComponent<ThirdPersonCharacter>();
+        if (hull != null)
+        {
+            //Physics.IgnoreCollision(this.GetComponent<Collider>(), other.GetComponent<Collider>());
+            hull.AttachToRope(this.gameObject);
+        }
+        //other.gameObject.transform.position = this.transform.position;
+        //Destroy(other.gameObject);
+    }
+
+}
---
+++ Assets/followAsh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followAsh : MonoBehaviour {
+	public GameObject character;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = character.transform.position.x;
+		float y = character.transform.position.y;
+		float z = this.transform.position.z;
+		this.transform.position = new Vector3 (x, y, z);
+	}
+}
